2c01547f2c7eed12ae9a818a46eb718728df7690
Bruno Henrique <brunohenriquesantos272@gmail.com>
2023-12-05 22:37:37 -0300

    feat: configs shadcn

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,15 +1,15 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/utils/shad-cn-configs'
 
 function Skeleton({
-  className,
-  ...props
+	className,
+	...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
-      {...props}
-    />
-  )
+	return (
+		<div
+			className={cn('animate-pulse rounded-md bg-primary/10', className)}
+			{...props}
+		/>
+	)
 }
 
 export { Skeleton }
