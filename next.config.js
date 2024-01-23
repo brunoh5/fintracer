c364a9eae623e484f8b7b4e74d1122001a206907
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-01-23 11:18:19 -0300

    fix: retirando config do MSW

--- next.config.js
+++ next.config.js
@@ -8,24 +8,6 @@ const nextConfig = {
 			},
 		],
 	},
-	webpack: (config, { isServer }) => {
-		if (isServer) {
-			if (Array.isArray(config.resolve.alias)) {
-				config.resolve.alias.push({ name: 'msw/browser', alias: false })
-			} else {
-				config.resolve.alias['msw/browser'] = false
-			}
-		} else {
-			if (Array.isArray(config.resolve.alias)) {
-				config.resolve.alias.push({ name: 'msw/node', alias: false })
-			} else {
-				config.resolve.alias['msw/node'] = false
-			}
-		}
-		return config
-	},
-	// productionBrowserSourceMaps: false,
-	// optimizeFonts: false,
 }
 
 module.exports = nextConfig
