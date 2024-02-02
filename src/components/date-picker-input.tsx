c89122edf6a4ba8a5c7a55f00bdcf62dbc14155f
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-02-02 00:52:56 -0300

    feat: corrigindo erros

--- src/components/date-picker-input.tsx
+++ src/components/date-picker-input.tsx
@@ -1,28 +1,28 @@
-'use client'
+// 'use client'
 
-import { format } from 'date-fns'
-import { CalendarIcon } from 'lucide-react'
+// import { format } from 'date-fns'
+// import { CalendarIcon } from 'lucide-react'
 
-import { cn } from '@/utils/shad-cn-configs'
+// import { cn } from '@/utils/shad-cn-configs'
 
-import { Button } from './ui/button'
-import { Calendar } from './ui/calendar'
-import {
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from './ui/form'
-import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+// import { Button } from './ui/button'
+// import { Calendar } from './ui/calendar'
+// import {
+// 	FormControl,
+// 	FormDescription,
+// 	FormField,
+// 	FormItem,
+// 	FormLabel,
+// 	FormMessage,
+// } from './ui/form'
+// import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
 
-interface DatePickerInputProps {
-	control: any
-}
+// interface DatePickerInputProps {
+// 	control: any
+// }
 
-export function DatePickerInput({ control }: DatePickerInputProps) {
-	return (
-		
-	)
-}
+// export function DatePickerInput({ control }: DatePickerInputProps) {
+// 	return (
+
+// 	)
+// }
