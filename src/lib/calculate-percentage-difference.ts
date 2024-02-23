50e3880c5a95d667c486a63e832de1cb0c878e56
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-02-23 12:16:03 -0300

    feat: adiciona um error handler

--- src/lib/calculate-percentage-difference.ts
+++ src/lib/calculate-percentage-difference.ts
@@ -10,6 +10,9 @@ export function calculatePercentageDifference(
 	value1: number,
 	value2: number,
 ): number {
+	if (value1 === 0) {
+		return 0
+	}
 	const difference = value2 - value1
 	const percentage = (difference / value1) * 100
 
