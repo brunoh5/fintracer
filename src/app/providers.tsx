eb769a70f911a7a01ead20c4781c4cc91b662baa
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-06-13 16:18:21 -0300

    deps: corrige pnpm-lock

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ReactNode, useState } from 'react'
@@ -15,18 +13,15 @@ export default function Providers({ children }: { children: ReactNode }) {
 	return (
 		<SessionProvider>
 			<QueryClientProvider client={queryClient}>
-				<ReactQueryStreamedHydration>
-					<NextThemesProvider
-						attribute="class"
-						defaultTheme="system"
-						enableSystem
-						disableTransitionOnChange
-					>
-						<DialogProvider />
-						{children}
-					</NextThemesProvider>
-				</ReactQueryStreamedHydration>
-				<ReactQueryDevtools initialIsOpen={false} />
+				<NextThemesProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<DialogProvider />
+					{children}
+				</NextThemesProvider>
 			</QueryClientProvider>
 		</SessionProvider>
 	)
